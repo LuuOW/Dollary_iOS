b3563d2f42903c2eddb193c2322bac6f3a98695e
Lucas A. Kemp3  ( ͡° ͜ʖ ͡°) <lucas.kempe@mercadolibre.com>
2021-10-18 17:05:19 -0300

    Adding ViewController ...

--- Dollary/Controller/ViewController.swift
+++ Dollary/Controller/ViewController.swift
@@ -10,4 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var employeeTableView: UITableView!
+    
+    private var curreyncyViewModel : CurrencyViewModel!
+    
+    private var dataSource : EmployeeTableViewDataSource<EmployeeTableViewCell,EmployeeData>!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        callToViewModelForUIUpdate()
+    }
+    
+    func callToViewModelForUIUpdate(){
+        
+        self.curreyncyViewModel =  CurrencyViewModel()
+        self.curreyncyViewModel.bindCurrencyViewModelToController = {
+            self.updateDataSource()
+        }
+    }
+    
+    func updateDataSource(){
+        
+        self.dataSource = EmployeeTableViewDataSource(cellIdentifier: "EmployeeTableViewCell", items: self.employeeViewModel.empData.data, configureCell: { (cell, evm) in
+            cell.employeeIdLabel.text = evm.id
+            cell.employeeNameLabel.text = evm.employeeName
+        })
+        
+        DispatchQueue.main.async {
+            self.employeeTableView.dataSource = self.dataSource
+            self.employeeTableView.reloadData()
+        }
+    }
+    
 }
