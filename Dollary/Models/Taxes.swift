97d13a38db6dfa3f456707a98abb6b2d6d841d44
Lucas A. Kemp3  ( ͡° ͜ʖ ͡°) <lucas.kempe@mercadolibre.com>
2021-10-23 21:40:36 -0300

    Adding taxes class...

---
+++ Dollary/Models/Taxes.swift
@@ -0,0 +1,17 @@
+//
+//  Taxes.swift
+//  Dollary
+//
+//  Created by Lucas Alejandro Kempe on 23/10/2021.
+//
+
+import Foundation
+
+struct Taxes {
+    
+    let imp21 = 0.21
+    let imp30 = 0.3
+    let imp35 = 0.35
+    
+    
+}
