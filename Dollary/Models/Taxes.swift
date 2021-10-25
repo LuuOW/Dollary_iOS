e783340be771dc57f8dc84fbc2b1cd3a09cb32de
Lucas A. Kemp3  ( ͡° ͜ʖ ͡°) <lucas.kempe@mercadolibre.com>
2021-10-24 23:19:53 -0300

    Adding more taxes...

--- Dollary/Models/Taxes.swift
+++ Dollary/Models/Taxes.swift
@@ -12,6 +12,15 @@ struct Taxes {
     let imp21 = 0.21
     let imp30 = 0.3
     let imp35 = 0.35
+    let imp50 = 0.5
+    
+    let impuestoTotalDigital = imp21 + imp30 + imp35
+    
+    let impuestoTotalFisico = imp30 + imp35
+    
+    let impuestoTotalFisico50 = imp30 + imp35 + imp50
+    
+    let impuestoTotalDivisa = imp30 + imp35
     
     
 }
